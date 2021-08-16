51feac708fd021811d62f82f226047d0f6c24304
Edisson Maldonado <edisson.maldonado@geniussports.com>
2021-08-16 17:25:09 -0500

    set loging and bind queue automatic

--- app/proxy.py
+++ app/proxy.py
@@ -4,12 +4,12 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 
-my_bucket = os.environ.get('BUCKET')
-db_token = os.environ.get('DBTOKEN')
-my_org = os.environ.get('ORG')
-rabbit_user = os.environ.get('RABBIT_USER')
-rabbit_password = os.environ.get('RABBIT_PASSWORD')
-
+my_bucket = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET")
+db_token = os.environ.get("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
+my_org = os.environ.get("DOCKER_INFLUXDB_INIT_ORG")
+rabbit_user = os.environ.get("RABBIT_USER")
+rabbit_password = os.environ.get("RABBIT_PASSWORD")
+queue_name  = "mensajes"
 
 client = InfluxDBClient(url="http://influxdb:8086", token=db_token, org=my_org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -17,23 +17,26 @@ query_api = client.query_api()
 
 def process_function(msg):
   mesage = msg.decode("utf-8")
-  print(mesage)
-  point = Point("temperatura").tag("place", "1").field("used_percent", float(mesage)).time(datetime.utcnow(), WritePrecision.NS)
+
+  string_list = mesage.split(",")
+  point = Point(string_list[1]).tag("place", "Santa marta").field(string_list[3], float(string_list[2])).time(datetime.utcnow(), WritePrecision.NS)
   write_api.write(my_bucket, my_org, point)
   return
 
 while 1:
-  url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:{}@{}:5672/%2f'.format(rabbit_user, rabbit_password))
+
+  url = os.environ.get('CLOUDAMQP_URL', 'amqp://{}:{}@rabbit:5672/%2f'.format(rabbit_user, rabbit_password))
   params = pika.URLParameters(url)
   connection = pika.BlockingConnection(params)
   channel = connection.channel() # start a channel
-  channel.queue_declare(queue='mensajes') # Declare a queue
+  channel.queue_declare(queue=queue_name) # Declare a queue
+  channel.queue_bind(exchange="amq.topic", queue=queue_name, routing_key='#')
   # create a function which is called on incoming messages
   def callback(ch, method, properties, body):
     process_function(body)
 
   # set up subscription on the queue
-  channel.basic_consume('mensajes', callback, auto_ack=True)
+  channel.basic_consume(queue_name, callback, auto_ack=True)
 
   # start consuming (blocks)
   channel.start_consuming()
